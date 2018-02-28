c469169ce782631f0989258823148710869fdb08
Jonathan Campbell <jonathan@stromata.com>
2018-02-28 15:08:01 -0500

    Add files via upload

--- WUIF/Headers/GFX/D2D/D2D.h
+++ WUIF/Headers/GFX/D2D/D2D.h
@@ -16,32 +16,35 @@ limitations under the License.*/
 #include <d2d1_3.h>     //needed for D2D resources
 #include <dwrite_3.h>   //needed for D2D/DWrite resources
 #include <wincodec.h>   //needed for IWICImagingFactory2 definition
+#include "GFX\D3D\D3D11.h"
 
 namespace WUIF {
     class Window;
 
-    class D2DResources
+    class D2DResources : public D3D11Resources
     {
     public:
-        D2DResources(Window&);
+        D2DResources(Window * const);
         ~D2DResources();
 
         //Direct2D objects
         static Microsoft::WRL::ComPtr<ID2D1Factory1> 	   d2dFactory;       //Direct2D factory access
         static Microsoft::WRL::ComPtr<ID2D1Device> 		   d2dDevice;        //Direct2D device
-        Microsoft::WRL::ComPtr<ID2D1DeviceContext>  d2dDeviceContext; //Direct2d render target (actually a device context)
-        Microsoft::WRL::ComPtr<ID2D1Bitmap1> 	   d2dRenderTarget;  //Direct2D target rendering bitmap
-                                                                     //DirectWrite objects
+        Microsoft::WRL::ComPtr<ID2D1DeviceContext>         d2dDeviceContext; //Direct2d render target
+        Microsoft::WRL::ComPtr<ID2D1Bitmap1> 	           d2dRenderTarget;  //Direct2D target rendering bitmap
+        //DirectWrite objects
         static Microsoft::WRL::ComPtr<IDWriteFactory1> 	   dwFactory; 	//DirectWrite factory access
         static Microsoft::WRL::ComPtr<IWICImagingFactory2> wicFactory;	//Windows Imaging Component (WIC) factory access
 
-                                                                        //functions
-        static void CreateStaticResources();
-        void CreateDeviceResources();
-        void ReleaseNonStaticResources();
+        static D2D1_FACTORY_TYPE   d2d1factorytype;
+        static DWRITE_FACTORY_TYPE dwfactorytype;
 
-        Window &window;
-    private:
-        
+        //functions
+        static void CreateD2DStaticResources();
+        void CreateD2DDeviceResources();
+        void ReleaseD2DNonStaticResources();
+
+        //Window &window;
+    protected:
     };
 }
