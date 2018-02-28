d94d1bda54e4b35bf0769722b8563c676e850025
Jonathan Campbell <jonathan@stromata.com>
2018-02-28 15:07:14 -0500

    Add files via upload

--- WUIF/Headers/Application/Application.h
+++ WUIF/Headers/Application/Application.h
@@ -12,50 +12,37 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.*/
 #pragma once
-#include <forward_list>
-#include <stdint.h> //needed for uint_fast8_t
+#include <vector>
+#include <unordered_map>
 #include <d3dcommon.h> //needed for D3D_FEATURE_LEVEL
-#include "..\Bitfield.h"
+#include <ShellScalingApi.h>
+#include "..\WUIF_Const.h"
 
 namespace WUIF {
-
-    namespace FLAGS {
-        BIT_FIELD(uint_fast8_t, GFX_Flags);
-        BIT_MASK(GFX_Flags, WARP,  1);
-        BIT_MASK(GFX_Flags, D2D,   2);
-        BIT_MASK(GFX_Flags, D3D11, 3);
-        BIT_MASK(GFX_Flags, D3D12, 4);
-    }
-
-    enum class OSVersion {
-        UNKNOWN = 0,
-        WIN7,
-        WIN8,
-        WIN8_1,
-        WIN10,
-        WIN10_1511, //WIN10_1511 - November Update
-        WIN10_1607, //WIN10_1607 - Anniversary Update
-        WIN10_1703, //WIN10_1703 - Creators Update
-        WIN10_1709, //WIN10_1709 - Fall Creators Update
-        WIN10_1803  //WIN10_1803 - ???
-    };
+    extern void __fastcall changeconst(_In_ void *var, _In_ const void *value);
 
     class Window;
 
     namespace App {
-        extern const int               nCmdShow;
-        extern const LPTSTR            lpCmdLine;
-        extern const HINSTANCE         hInstance;
-        extern const OSVersion         winversion; //which version of Windows OS app is running on
-        extern const FLAGS::GFX_Flags  GFXflags;   //flags defined in code stating which configuration to run - declared in WUIF_Main.h
-        extern const D3D_FEATURE_LEVEL minD3DFL;   //minimum D3DFL for the application - declared in WUIF.h
+        extern const volatile int              nCmdShow;
+        extern const volatile LPTSTR           lpCmdLine;
+        extern const volatile HINSTANCE        hInstance;
+        extern const volatile OSVersion        winversion; //which version of Windows OS app is running on
+        extern const volatile FLAGS::GFX_Flags GFXflags;   //flags defined in code stating which configuration to run - declared in WUIF_Main.h
+        extern const volatile PROCESS_DPI_AWARENESS * const volatile processdpiawareness; //pointer to value of processes' PROCESS_DPI_AWARENESS
+        extern const volatile DPI_AWARENESS_CONTEXT * const volatile processdpiawarenesscontext; //pointer to value of processes' DPI_AWARENESS_CONTEXT
+        extern const D3D_FEATURE_LEVEL  minD3DFL;   //minimum D3DFL for the application - declared in WUIF.h
 
         extern       Window            *mainWindow;
-        extern       std::forward_list<Window*> Windows;
 
-        extern       HINSTANCE         libD3D12;
+        extern       HINSTANCE          libD3D12;
 
         //functions
+        extern inline const std::vector<Window*>& GetWindows();
         extern void(*ExceptionHandler)(void);
+
+        /*Global user WindowProcedure function - this is a general WndProc for all windows of the
+        application*/
+        extern std::unordered_map<int, WNDPROC> GWndProc_map;
     };
 }
