c983ce3fcf573373429511ea1a00c150ae07f04d
Jonathan Campbell <jonathan@stromata.com>
2018-02-28 15:14:28 -0500

    Add files via upload

---
+++ WUIF/Sample/Resources/resource.h
@@ -0,0 +1,19 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Sample.rc
+//
+#define IDR_MENU1                       103
+#define ID_FILE_EXIT                    40003
+#define ID_HELP_ABOUT                   40004
+#define ID_FILE_CHANGEICON              40005
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         40006
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
